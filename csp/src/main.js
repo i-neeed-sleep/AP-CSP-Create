c5a64a98140ae534397ca36c5448aa93382d4bf6
i-neeed-sleep <kammyl@nycstudents.net>
2026-01-12 14:02:18 -0500

    eeeeeeeeeeeeee

---
+++ csp/src/main.js
@@ -0,0 +1,9 @@
+
+const list = document.querySelector("#list");
+const input = document.querySelector("#input")
+const submit = document.querySelector("#submit")
+const preview = document.querySelector("#preview")
+
+function e(){
+
+}
