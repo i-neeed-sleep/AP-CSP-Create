efc497d0d38bd1b7c46f01931f2c4249a469d811
i-neeed-sleep <kammyl@nycstudents.net>
2026-01-16 14:00:08 -0500

    weeeeeeeeeee

--- csp/src/main.js
+++ csp/src/main.js
@@ -3,34 +3,37 @@ const list = document.querySelector("#list");
 const input = document.querySelector("#input");
 const submit = document.querySelector("#submit");
 
-const preset =[{
-    name: 'Red',
-    code: '#ff0000'
+let set =[{
+    'name': 'Red',
+    'code': '#ff0000'
 },{
-    name: 'Green',
-    code: '#00ff00'
+    'name': 'Green',
+    'code': '#00ff00'
 },{
-    name: 'Blue',
-    code: '#0000ff'
-}
-] 
+    'name': 'Blue',
+    'code': '#0000ff'
+}] 
 
-preset.forEach(color =>{
-    document.querySelector('#list').insertAdjacentHTML('afterbegin',`
-        <div id = "card">
-            <div id="color" style="background-color:${color.code};"></div>
-            <h1>${color.name} - ${color.code}</h1>
-            <button id='close'>Clean from Palette</button>
-        </div>
-    `)
-})
+function start(list){
+    list.forEach(color =>{
+        document.querySelector('#list').insertAdjacentHTML('afterbegin',`
+            <div class = "card" id="${color.code}">
+                <div id="color" style="background-color:${color.code};"></div>
+                <h1>${color.name} - ${color.code}</h1>
+                <button id = 'close'>Clean from Palette</button>
+            </div>
+        `)
+    })
+}
+start(set);
+
 
 function add(rgb, name){
     document.querySelector('#list').insertAdjacentHTML('afterbegin',`
-        <div id = "card">
+        <div class = "card" id = '${rgb}'>
             <div id="color" style="background-color:${rgb};"></div>
             <h1>${name} - ${rgb}</h1>
-            <button id='close'>Clean from Palette</button>
+            <button id = 'close'>Clean from Palette</button>
         </div>
     `)
 }
@@ -40,7 +43,6 @@ function error(){
 }
 
 document.querySelector('#submit').addEventListener("click", sub =>{
-    sub.preventDefault();
     let color = document.querySelector('#input').value;
     let name = document.querySelector('#inName').value;
     if (color.length > 7 || color.lenght < 6){
@@ -49,13 +51,22 @@ document.querySelector('#submit').addEventListener("click", sub =>{
         if (color.includes('#')){
             add(color, name);
             document.querySelector('#message').innerHTML = '';
+            set.push({'name': name, 'code': color})
         }
         if (color.includes('#') != true & color.length === 6){
             let c = '#' + color;
             add(c, name);
             document.querySelector('#message').innerHTML = '';
+            set.push({'name': name, 'code': c})
         }else{
             error();
         }
     }
 }) 
+
+document.querySelectorAll('#close').forEach(i => addEventListener("click", close =>{
+    const target = close.target.closest(".card").getAttribute('id');
+    document.querySelector('#list').innerHTML = '';
+    set = set.filter(i => i.code !== target);
+    start(set);
+}))
